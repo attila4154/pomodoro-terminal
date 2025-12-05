49db9e17179b4d40e593279ed817a52456bf556c
Danila Makulov <attila415422@gmail.com>
2025-12-05 15:09:12 +0100

    add key press highlight in footer

--- source/components/ActionsFooter.tsx
+++ source/components/ActionsFooter.tsx
@@ -1,17 +1,26 @@
-import {Box, Text} from 'ink';
-import React, {useContext} from 'react';
+import {Box, Text, useInput} from 'ink';
+import React, {useContext, useState} from 'react';
 import {KeyPressActionContext} from '../context/KeyPressActionContext.js';
 
 function ActionsFooter() {
 	const {actions} = useContext(KeyPressActionContext);
+	const [pressedKey, setPressedKey] = useState<string | null>(null);
+
+	useInput(input => {
+		const actionExists = actions[input] !== undefined;
+		if (actionExists) {
+			setPressedKey(input);
+			setTimeout(() => setPressedKey(null), 300);
+		}
+	});
 
 	return (
 		<Box height={1} borderTop justifyContent="center" gap={3}>
 			{Object.entries(actions).map(([key, [description]]) => (
 				<Text
 					key={key}
-					// bold={pressedButton === key}
-					// dimColor={pressedButton === key}
+					dimColor={pressedKey === key}
+					color={pressedKey === key ? 'blue' : undefined}
 				>
 					[{key}]: {description}
 				</Text>
